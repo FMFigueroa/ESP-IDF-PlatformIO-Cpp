718a52d51a823b53fca675f42c1f7da1e9246161
Felix Figueroa <felixmanuelfigueroa@gmail.com>
2022-08-13 22:05:46 -0300

    Class with setup & loop function

--- src/main.cpp
+++ src/main.cpp
@@ -15,15 +15,32 @@
 #include "esp_log.h"
 #define LOG_TAG "MAIN"
 
-
+static Main my_main;
 
 extern "C" void app_main(void)
 {
 
+    ESP_ERROR_CHECK(my_main.setup());
+
+    while (true)
+    {
+        my_main.loop();
+    }
+ 
+}
+
+esp_err_t Main::setup(void)
+{
+    esp_err_t status{ESP_OK};
+    ESP_LOGI(LOG_TAG, "Setup function oK");
+    return status;
+}
+
+void Main::loop(void)
+{
     while(true)
     {
-        ESP_LOGI(LOG_TAG, "Hello World!");
+        ESP_LOGI(LOG_TAG, "Hello World!🚀");
         vTaskDelay(pdSECOND);
     }
-    
 }
